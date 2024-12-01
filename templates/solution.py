a88183da88efcb4237bfc2c3ba90223174911dec
Kian Boon <kbbbtan@gmail.com>
2024-11-30 21:14:45 -0800

    Added templates for md and py files

---
+++ templates/solution.py
@@ -0,0 +1,27 @@
+"""
+Contains solutions for Day N stars.
+Run on Python 3.8.3.
+"""
+from collections import defaultdict, deque
+
+INPUT_FILE = "input.txt"
+
+def star_1():
+    """ Solution for Star 1.
+    """
+    with open(INPUT_FILE) as file:
+        lines = file.read().split("\n")
+    
+def star_2():
+    """ Solution for Star 2.
+    """
+    with open(INPUT_FILE) as file:
+        lines = file.read().split("\n")
+    
+def main():
+    """ Contains driver code for running solutions.
+    """
+    print("Solution for Star 1: ", star_1())
+    print("Solution for Star 2: ", star_2())
+
+main()
